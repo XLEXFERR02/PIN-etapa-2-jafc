64672ff1a6509d53bf86ec71b457f8514b99c958
xlexferr02 <xlexferr02@hotmail.com>
2025-02-02 18:41:22 -0500

    Actualizar backend.tf y bootstrap_variables.tf con nuevos valores

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3"{
-    bucket                 = "mundose22"
+    bucket                 = "mundose2403jafc"
     region                 = "us-east-1"
     key                    = "backend.tfstate"
     dynamodb_table         = "terraformstatelock"
--- modules/bootstrap/bootstrap_variables.tf
+++ modules/bootstrap/bootstrap_variables.tf
@@ -1,10 +1,30 @@
 ##
 # Define variables for Azure DevOps Seed Module
 ##
+variable "name_of_s3_bucket" {
+  description = "Nombre del bucket S3 donde se almacenará el estado de Terraform"
+  default     = "mundose2403jafc"
+}
 
-variable "name_of_s3_bucket" {}
-variable "dynamo_db_table_name" {}
-variable "iam_user_name" {}
-variable "ado_iam_role_name" {}
-variable "aws_iam_policy_permits_name" {}
-variable "aws_iam_policy_assume_name" {}
+variable "dynamo_db_table_name" {
+  description = "Nombre de la tabla DynamoDB para el bloqueo del estado de Terraform"
+  default     = "terraformstatelock"
+}
+
+variable "iam_user_name" {
+  description = "Nombre del usuario IAM que se utilizará"
+  default     = "xlexferr02"
+}
+
+# Las siguientes variables no son necesarias en este ejercicio (son para configuraciones de Azure o políticas IAM que no se usan)
+# variable "ado_iam_role_name" {
+#   description = "Nombre del rol IAM para Azure DevOps"
+# }
+
+# variable "aws_iam_policy_permits_name" {
+#   description = "Nombre de la política IAM con permisos específicos"
+# }
+
+# variable "aws_iam_policy_assume_name" {
+#   description = "Nombre de la política IAM para asumir roles"
+# }
